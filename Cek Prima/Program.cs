8e59102a0696e83c377aba5d480ea3573fca9e46
LisandraN08 <lnicoline@student.ciputra.ac.id>
2021-12-03 10:33:04 +0700

    Add project files.

---
+++ Cek Prima/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cek_Prima
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
