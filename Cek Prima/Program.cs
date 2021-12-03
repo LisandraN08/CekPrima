a398ea78753cbdb20186c4b6a8490df3085f300e
LisandraN08 <lnicoline@student.ciputra.ac.id>
2021-12-03 10:34:29 +0700

    Menghitung jumlah faktor input

--- Cek Prima/Program.cs
+++ Cek Prima/Program.cs
@@ -8,7 +8,14 @@ namespace Cek_Prima
         {
             Console.Write("Masukkan angka: ");
             int Bilangan = Convert.ToInt32(Console.ReadLine());
-            
+            int jumlahFaktor = 0;
+            for (int i = 1; i <= Bilangan; i++)
+            {
+                if (Bilangan % i == 0)
+                {
+                    jumlahFaktor += 1;
+                }
+            }
         }
     }
 }
